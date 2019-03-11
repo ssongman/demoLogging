58eee6057b18749a778548c2b4dd2b0fcd80f821
ssongman <ssongmantop@gamil.com>
2019-03-11 10:37:07 +0900

    fisrt commit

---
+++ src/main/java/com/ssongman/DemoLoggingApplication.java
@@ -0,0 +1,14 @@
+package com.ssongman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLoggingApplication.class, args);
+	}
+
+}
+
